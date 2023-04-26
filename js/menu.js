b9d587bef702c32a20525944c7b486be7ebfa944
Seba HA <hereniu.amaral.sebastian@gmail.com>
2023-04-26 14:42:37 -0300

    Version 0.1

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,13 @@
-const nav = document.querySelector(".navbar");
+let nav = document.getElementById("nav");
+let menu = document.getElementById("menu-lg");
 
-window.addEventListener("scroll", function () {
-	navbar.classList.toggle("active", window.scrollY > 0);
+window.addEventListener("scroll", () => {
+	let value = window.scrollY;
+	if (value > 0) {
+		nav.classList.add("nav-scrolled");
+		menu.classList.add("menu-lg-scrolled");
+	} else {
+		nav.classList.remove("nav-scrolled");
+		menu.classList.remove("menu-lg-scrolled");
+	}
 });
