66e4a1548ed5525959f35f71780f672fdfef6aa7
Seba HA <hereniu.amaral.sebastian@gmail.com>
2023-05-16 14:31:51 -0300

    Cambios de medias querys y php

--- js/app.js
+++ js/app.js
@@ -3,9 +3,3 @@ const nav = document.querySelector(".navbar");
 window.addEventListener("scroll", function () {
 	nav.classList.toggle("active", window.scrollY > 0);
 });
-
-
-// Path: js\app.js
-
-
-
